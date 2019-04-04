26bb5c6aae6779dfa9c8610ec936f4a55fb1b931
luxxluciano <luxx.tv@gmail.com>
2019-04-04 14:34:56 -0300

    atualiza biblioteca

--- listaDuplaEncadeada.py
+++ listaDuplaEncadeada.py
@@ -39,9 +39,36 @@ class Lista:
 			no.prev=self.tail
 			self.tail=self.tail.next
 
-	def remove (self):
-		self.tail=self.tail.prev
-		self.tail.next=self.tail.next
+	def remover(self, dado):
+        """ Remove um no da lista. """
+        # O no atual eh o primeiro no da lista
+        no_atual = self.head
+ 
+        # Vamos procurar pelo dado que queremos remover
+        # Equanto o no atual for valido
+        while no_atual is not None:
+            # Verifica se eh o dado que estamos buscando
+            if no_atual.dado == dado:
+                # Se o dado que estamos buscando esta no primeiro no
+                # da lista, nao temos anterior
+                if no_atual.prev is None:
+                    # A cabeca 'aponta' para o proximo no da lista
+                    self.head = no_atual.next
+                    # E o anterior do proximo no aponta para None
+                    no_atual.next.prev = None
+                else:
+                    # Exemplo: Removendo o valor 5
+                    # ... <---> | 2 | <---> | 5 | <---> | 12 | <---> ...
+                    #
+                    # O proximo do valor 2 passa a apontar para o 12 e
+                    # o anterior do valor 12 passa a apontar para o 2
+                    #                     ---------------
+                    # ... <---> | 2 | <---|--- | 5 | ---|---> | 12 | <---> ... 
+                    no_atual.prev.next = no_atual.next
+                    no_atual.next.prev = no_atual.prev
+
+            # Se nao eh o no que estamos buscando va para o proximo
+            no_atual = no_atual.next
 
 
 lista= Lista()
